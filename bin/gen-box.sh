b5e34888f297155aaf478d9fc241b19bd4d83f85
Douglas Atique <datique@macmini2.int.d3atiq.net>
2019-03-22 00:24:17 -0400

    Changed mechanism to download vagrant insecure key.

--- bin/gen-box.sh
+++ bin/gen-box.sh
@@ -2,5 +2,5 @@
 
 BASEDIR=$(cd "$(dirname "$0")"; pwd)
 
-mkdir -p $BASEDIR/../target/.ssh &&\
+$BASEDIR/download-vagrant-insecure-key.sh
 $BASEDIR/packer build -var "vagrant-cloud-token=$(cat $BASEDIR/../etc/vagrant-cloud.token)" "$@" 
