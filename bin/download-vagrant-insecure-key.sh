e0d0e7655352a8b2a74632b65c016e960142a53e
Douglas Atique <d3atiq@gmail.com>
2019-04-09 13:14:06 -0400

    Added net-tools package. Download of vagrant insecure private key (will not be needed for ansible provisioning, after all).

--- bin/download-vagrant-insecure-key.sh
+++ bin/download-vagrant-insecure-key.sh
@@ -5,3 +5,4 @@ BASEDIR=$(cd "$(dirname "$0")"; pwd)
 mkdir -p $BASEDIR/../.ssh
 
 curl -so $BASEDIR/../.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
+curl -so $BASEDIR/../.ssh/vagrant.key https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant
